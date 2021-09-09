e7cdb5ea17519802a79745d6dadc17b7582d366a
anurag47103 <anuragsharma47103@gmail.com>
2021-09-09 16:41:09 +0530

    initial commit with basic UI

---
+++ app/src/main/java/com/bawp/todoister/MainActivity.java
@@ -0,0 +1,67 @@
+package com.bawp.todoister;
+
+import android.os.Bundle;
+
+import com.bawp.todoister.model.Priority;
+import com.bawp.todoister.model.Task;
+import com.bawp.todoister.model.TaskViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TaskViewModel taskViewModel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        taskViewModel = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this.getApplication())
+                .create(TaskViewModel.class);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Task task = new Task("todo", Priority.HIGH, Calendar.getInstance().getTime(),
+                    Calendar.getInstance().getTime(),
+                    false);
+
+            TaskViewModel.insert(task);
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
