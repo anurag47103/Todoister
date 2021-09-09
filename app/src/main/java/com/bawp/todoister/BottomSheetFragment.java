2d83f7a6a98da1c9cc608ef7dcab0b1eb474a836
anurag47103 <anuragsharma47103@gmail.com>
2021-09-10 01:09:07 +0530

    saving a task from BottomSheetFragment (taskName and dueDate)

--- app/src/main/java/com/bawp/todoister/BottomSheetFragment.java
+++ app/src/main/java/com/bawp/todoister/BottomSheetFragment.java
@@ -2,6 +2,7 @@ package com.bawp.todoister;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,8 +24,9 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import java.util.Calendar;
+import java.util.Date;
 
-public class BottomSheetFragment extends BottomSheetDialogFragment {
+public class BottomSheetFragment extends BottomSheetDialogFragment implements View.OnClickListener {
 
     private EditText enterTodo;
     private ImageButton priorityButton;
@@ -35,6 +37,8 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     private ImageButton saveButton;
     private CalendarView calendarView;
     private Group calendarGroup;
+    private Date dueDate;
+    Calendar calendar = Calendar.getInstance();
 
     public BottomSheetFragment() {
     }
@@ -56,21 +60,56 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
         priorityButton = view.findViewById(R.id.priority_todo_button);
 
         Chip todayChip = view.findViewById(R.id.today_chip);
+        todayChip.setOnClickListener(this);
         Chip tomorrowChip = view.findViewById(R.id.tomorrow_chip);
+        tomorrowChip.setOnClickListener(this);
         Chip nextWeekChip = view.findViewById(R.id.next_week_chip);
+        nextWeekChip.setOnClickListener(this);
         return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        calenderButton.setOnClickListener(v -> {
+            calendarGroup.setVisibility(
+                    calendarGroup.getVisibility() == View.GONE ? View.VISIBLE : View.GONE
+            );
+        });
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                calendar.clear();
+                calendar.set(year , month , dayOfMonth);
+                dueDate = calendar.getTime();
+            }
+        });
+
         saveButton.setOnClickListener(v -> {
             String task = enterTodo.getText().toString().trim();
-            if(!TextUtils.isEmpty(task)) {
-                Task mytask = new Task(task, Priority.HIGH, Calendar.getInstance().getTime(),
+            if(!TextUtils.isEmpty(task) && dueDate != null) {
+                Task mytask = new Task(task, Priority.HIGH, dueDate,
                         Calendar.getInstance().getTime(), false);
                 TaskViewModel.insert(mytask);
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.today_chip) {
+            calendar.add(Calendar.DAY_OF_YEAR , 0);
+            dueDate = calendar.getTime();
+        }
+        else if(id == R.id.tomorrow_chip) {
+            calendar.add(Calendar.DAY_OF_YEAR , 1);
+            dueDate = calendar.getTime();
+        }
+        else if(id == R.id.next_week_chip) {
+            calendar.add(Calendar.DAY_OF_YEAR , 2);
+            dueDate = calendar.getTime();
+        }
     }
 }
